430d386b39e8b891830cee079bb54878a394ffa2
Dimitri Tkavadze <dtkav22@freeuni.edu.ge>
2024-07-28 16:36:46 +0400

    added security login with jwt token

---
+++ src/main/java/com/example/Website/dao/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.Website.dao;
+
+import com.example.Website.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+	User findByUsername(String username);
+}
